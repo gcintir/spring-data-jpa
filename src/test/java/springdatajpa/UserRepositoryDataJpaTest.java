578461397a5fa1d2c9da54b65c92e388e2eeff5d
gurayci <guray.cintir@argela.com.tr>
2019-05-15 01:55:13 +0300

    unit testing & integration testing

--- src/test/java/springdatajpa/UserRepositoryDataJpaTest.java
+++ src/test/java/springdatajpa/UserRepositoryDataJpaTest.java
@@ -1,14 +1,12 @@
 package springdatajpa;
 
 import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import springdatajpa.model.User;
 import springdatajpa.repository.UserRepository;
 
